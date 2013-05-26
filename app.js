089ee1a2ca5a45cb635918c0db53933ec95d64f1
yalamber subba <yalamber@mundhum.com>
2013-05-26 23:37:45 +0545

    fix

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
-var express = require('express')
+var chatters = []
+  , chatters_socket = {}
+  , express = require('express')
   , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
@@ -20,8 +22,39 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('msg', { msg: "Welcome to 2linko" });
+  socket.emit('msg', { nickname: "2linko", msg: "Welcome to 2linko" });
+  socket.on('set nickname', function (data) {
+    if(data.nickname){
+      //check if nickname is not already taken
+      if(chatters.indexOf(data.nickname) === -1){
+        socket.set('nickname', data.nickname, function () {
+          chatters.push(data.nickname);
+          chatters_socket[socket.id] = data.nickname;
+          console.log(chatters);
+          socket.get('nickname', function(err, nickname) {
+            socket.emit('nickname set', {status: "success", nickname: nickname});
+          });
+        });
+      }
+      else{
+        socket.emit('nickname set', {status: "failed"});
+      }
+    }
+    else{
+      socket.emit('nickname set', {status: "failed"});
+    }
+  });
+
   socket.on('send', function (data) {
     io.sockets.emit('msg', data);
   });
+
+  socket.on('disconnect', function () {
+    socket.get('nickname', function(err, nickname) {
+      delete chatters_socket[socket.id];
+      delete chatters[chatters.indexOf(nickname)];
+    });
+    io.sockets.emit('user disconnected');
+  });
 });
+
--- public/js/main.js
+++ public/js/main.js
@@ -3,24 +3,61 @@ $(window).load(function() {
   var socket = io.connect(socket_server_address);
   var msg_list = $("#message_list");
   var msg_container = $("#message_container");
+  //ask nickname
+  $('#ask_nickname').modal({show:true, backdrop:"static", keyboard: false });
+  $('#ask_nickname').on('shown', function () {
+    $('#nickname').focus();
+  })
+  //save nickname
+  $('#save_nickname').click(function(){
+    socket.emit('set nickname', {nickname: $('#nickname').val()});
+    socket.on('nickname set', function(data){
+      if(data.status == 'success'){
+        $('#control_name').text(data.nickname);
+        $('#ask_nickname').modal('hide');
+      }
+      else{
+        $('#set_name_status').fadeIn().html('Nickname is not available!');
+      }
+    });
+  });
   socket.on('msg', function (data) {
     if(data.msg) {
       var html;
       var hue = 'rgba(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',.8)';
-      html = '<li style="background:'+hue+'">'+ data.msg + '</li>';
+      var nickname = data.nickname || '';
+      html = '<li style="background:'+hue+'"><strong>'+ nickname +'</strong> : '+ data.msg + '</li>';
       msg_list.append(html);
       msg_container.scrollTop(msg_container.height());
     } else {
       console.log("There is a problem:", data);
     }
   });
+
   $("#msg").keyup(function (e) {
-    setTimeout(function(){
-      socket.emit('typing', {type: "typing..."});
-    }, 5);
     if (e.keyCode == 13) {
-      socket.emit('send', { msg: $("#msg").val() });
-      $('#msg').val('');
+      if($('#nickname').val()!=''){
+        socket.emit('send', {nickname: $('#nickname').val(), msg: $("#msg").val()});
+        $('#msg').val('');
+      }
+      else{
+        alert('Please provide your name!');
+      }
     }
   });
+
+  $("#nickname").keyup(function (e) {
+    if (e.keyCode == 13) {
+      if($('#nickname').val()!=''){
+        $('#save_nickname').trigger("click");
+      }
+      else{
+        alert('Please provide your name!');
+      }
+    }
+  });
+
+});
+$(window).bind('beforeunload', function(){
+  return 'All messages will be lost, Are you sure you want to leave?';
 });
